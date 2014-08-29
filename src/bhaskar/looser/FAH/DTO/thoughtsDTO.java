7a730c564c27f77177b8664ae5d5054dea1e1ff4
bhaskey <anilbhaskar.cse@gmail.com>
2014-08-29 17:34:10 +0530

    DTO(data transfer object) package added

---
+++ src/bhaskar/looser/FAH/DTO/thoughtsDTO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bhaskar.looser.FAH.DTO;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author 360758
+ */
+public class thoughtsDTO {
+    private SimpleDateFormat date  = new SimpleDateFormat("ddmmyyyy");
+    private String thought;
+    
+    
+}
